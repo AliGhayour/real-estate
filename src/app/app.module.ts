f8217dd9993ad82e7712fef9422c7a6fd847d2c0
BitBucketAliGhayourMohatat <ghayour78@yahoo.com>
2020-07-24 19:31:11 +1000

    my forth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NotFoundComponent
+        NotFoundComponent,
+        HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent  },
+      { path: '**', component: NotFoundComponent  },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
